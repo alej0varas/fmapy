dd3317049bf3364819b80e82d6aeb248c7a85239
Alexandre Varas <alej0varas@gmail.com>
2016-12-05 12:52:00 +0100

    Added continuous playback

--- fmap.py
+++ fmap.py
@@ -2,6 +2,7 @@ import os
 import requests
 import requests_cache
 import tempfile
+import threading
 
 requests_cache.install_cache()  # :)
 
@@ -76,40 +77,70 @@ class Browser:
 
 
 class Player:
+
+    t = None
+
     def __init__(self):
-        b = Browser()
-        for index, genre in enumerate(b.genres):
+        self.q = False
+        self.b = Browser()
+        for index, genre in enumerate(self.b.genres):
             print(index, genre['genre_title'])
         option = int(input('Choose a genre: '))
-        b.set_genre(option)
+        self.b.set_genre(option)
 
         import pygame
         pygame.mixer.init()
-        m = pygame.mixer.music
+        self.m = pygame.mixer.music
+        self.t_stop = threading.Event()
 
+    def run(self):
         option = 'p'
-        while option is not None:
+        while not self.q:
             if option == 'q':
-                m.stop()
-                option = None
+                self.stop()
                 continue
             if option == 'n':
                 option = 'p'
             if option == 'p':
-                b.get_next_track()
-                track = b._get_content(b._get_track_url())
-                tmp_track = tempfile.NamedTemporaryFile()
-                tmp_track.write(track.content)
-                # # logging
-                # print(self.track['track_title'])
-                # print(self.track['artist_name'])
-                # print(self.track['album_title'])
-
-                m.load(tmp_track.name)
-                m.play()
-
+                self.play()
             option = input('>> ')
+
+    def play(self):
+        self.b.get_next_track()
+        track = self.b._get_content(self.b._get_track_url())
+        tmp_track = tempfile.NamedTemporaryFile()
+        tmp_track.write(track.content)
+        print(self.b.track['track_title'], '|',
+              self.b.track['artist_name'], '|',
+              self.b.track['album_title'])
+        self.m.load(tmp_track.name)
+        self.m.play()
+        if self.t is None:
+            self._play_thread()
+
+    def stop(self):
+        self.m.stop()
+        self.t_stop.set()
+        self.q = True
+
+    def _play_thread(self):
+
+        class PlayThread(threading.Thread):
+            def __init__(self, *args, **kwargs):
+                self.p = kwargs['kwargs']['p']
+                self.q = kwargs['kwargs']['q']
+                super(PlayThread, self).__init__(*args, **kwargs)
+
+            def run(self):
+                while not self.q.is_set():
+                    if not self.p.m.get_busy():
+                        self.p.play()
+                    self.q.wait(.5)
+
+        self.t = PlayThread(kwargs={'p': self, 'q': self.t_stop})
+        self.t.start()
 
 
 if __name__ == '__main__':
     p = Player()
+    p.run()
